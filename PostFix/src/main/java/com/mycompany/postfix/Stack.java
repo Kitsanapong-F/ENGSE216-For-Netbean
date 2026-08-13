874c779d9734e58613b0eb632641e9e62045edfb
Kitsanapong-F <09chaisu@gmail.com>
2026-08-14 00:01:14 +0700

    add Stack

--- PostFix/src/main/java/com/mycompany/postfix/Stack.java
+++ PostFix/src/main/java/com/mycompany/postfix/Stack.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.postfix;
-
-/**
- *
- * @author User
- */
+import java.util.Arrays;
 public class Stack {
+
+    private int t = -1;
+    private int arr[];
+
+    public Stack() {
+        this(10);
+    }
+
+    public Stack(int size) {
+        this.arr = new int[size];
+    }
     
+    public void push (int item){
+        if(isFull()){
+            arr = Arrays.copyOf(arr, arr.length * 2);
+        }
+    }
+    
+    public int pop(){
+        return 0;
+    }
+    
+    public boolean isFull(){
+        return  size() == arr.length;
+    }
+    
+    public boolean isEmpty(){
+        return t < 0;
+    }
+    
+    public int size(){
+        return t+1;
+    }
 }
